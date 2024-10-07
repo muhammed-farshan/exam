bcf207a45f5c1027dc9bb460a3731c7ee424d60b
muhammed-farshan <farshanneyyathoor007@gmail.com>
2024-10-07 13:05:28 +0530

    first commit

---
+++ JAVA SCRIPT/display.js
@@ -0,0 +1,7 @@
+// display contents 
+console.log(typeof"this is a js page")
+console.log(typeof 500);
+console.log(typeof false);
+
+// 
+console.log();
---
+++ JAVA SCRIPT/var.js
@@ -0,0 +1,13 @@
+var firstData
+console.log(firstData,typeof firstData);
+var firstData = 100
+console.log(firstData,typeof firstData);
+var firstData = "js"
+console.log(firstData,typeof firstData);
+
+// 
+console.log(secondData);
+const secondData ="seconddata"
+console.log(secondData);
+
+
